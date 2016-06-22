fbbfc284722cd79196d5ca4ba12460ecbb0d1dda
roman <roman@rkeller.net>
2016-06-22 19:04:44 +0200

    CGNS boundary conditions

--- test/flate_plate/gridgen.F90
+++ test/flate_plate/gridgen.F90
@@ -20,13 +20,14 @@ integer, parameter :: bc(4) = (/-2,-3,-1,-4/)
 
 real(kind=8) :: xyz (imax,jmax,kmax,Dimen)
 real(kind=8),allocatable :: vec (:,:,:,:)
-real(kind=8) :: p,gamma,rho,u,v,gm1,T,dy,Ma
+real(kind=8) :: p,gamma,rho,u,v,gm1,t,dy,ma
 
 integer :: i,j
 
-integer(kind = CGSIZE_T) :: ierror,cgns_file,cgns_base,cgns_zone,cgns_coord,cgns_var,cgns_sol
+integer(kind = CGSIZE_T) :: ierror,cgns_file,cgns_base,cgns_zone,cgns_coord,cgns_var,cgns_sol,cgns_bc
 
 integer(kind=CGSIZE_T) :: isize (dimen,3)
+integer(kind=CGSIZE_T) :: ibc   (dimen,2,6)
 
 write(*,*) "AIRFOIL converter"
 dy = 1.0D-4
@@ -45,22 +46,22 @@ do i = 1,imax
     end do
 end do
 allocate(vec (imax-1,jmax-1,kmax-1,nVar+2 ))
-Gamma  = 1.4D0
+gamma  = 1.4D0
 gm1 = gamma - 1.0D0
-T = 388.889D0
-Ma = 0.1D0
+t = 388.889D0
+ma = 0.1D0
 p = 41368.5D0
-rho = p / (RGAS * T)
-u = Ma * sqrt( gamma * RGas * T) 
+rho = p / (RGAS * t)
+u = ma * sqrt( gamma * RGAS * t) 
 v = 0.0D0
-write(*,*) rho,p,T,u
+write(*,*) rho,p,t,u
 
 vec(:,:,:,1) = rho
 vec(:,:,:,2) = u
 vec(:,:,:,3) = v
 vec(:,:,:,4) = p / gm1 + 0.5D0*rho*(u*u+v*v)
 vec(:,:,:,5) = p
-vec(:,:,:,6) = T
+vec(:,:,:,6) = t
 call cg_open_f("data_in.cgns",CG_MODE_WRITE,cgns_file,ierror)
 if (ierror /= CG_OK) call cg_error_exit_f()
 call cg_base_write_f(cgns_file,"Grid",Dimen,Dimen,cgns_base,ierror)
@@ -80,12 +81,24 @@ isize(3,3) = 0
 
 call cg_zone_write_f(cgns_file,cgns_base,"Zone 1",isize,Structured,cgns_zone,ierror)
 if (ierror /= CG_OK) call cg_error_exit_f()
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!            write grid                            !!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
 call cg_coord_write_f(cgns_file,cgns_base,cgns_zone,RealDouble,"CoordinateX",xyz(:,:,:,1),cgns_coord,ierror)
 if (ierror /= CG_OK) call cg_error_exit_f()
 call cg_coord_write_f(cgns_file,cgns_base,cgns_zone,RealDouble,"CoordinateY",xyz(:,:,:,2),cgns_coord,ierror)
 if (ierror /= CG_OK) call cg_error_exit_f()
 call cg_coord_write_f(cgns_file,cgns_base,cgns_zone,RealDouble,"CoordinateZ",xyz(:,:,:,3),cgns_coord,ierror)
 if (ierror /= CG_OK) call cg_error_exit_f()
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!            write solution                        !!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 call cg_sol_write_f(cgns_file,cgns_base,cgns_zone,"0",CellCenter,cgns_sol,ierror)
 if (ierror /= CG_OK) call cg_error_exit_f()
@@ -103,7 +116,65 @@ call cg_field_write_f(cgns_file,cgns_base,cgns_zone,cgns_sol,RealDouble         
 call cg_field_write_f(cgns_file,cgns_base,cgns_zone,cgns_sol,RealDouble         &
          ,"Energie",vec(:,:,:,4),cgns_var,ierror)
 
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!            write solution                        !!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
+!!! WEST INFLOW
+ibc(1,1,1)  = 1
+ibc(2,1,1)  = 1
+ibc(3,1,1)  = 1
+ibc(1,2,1)  = 1
+ibc(2,2,1)  = jmax - 1
+ibc(3,2,1)  = kmax - 1
+call cg_boco_write_f(cgns_file,cgns_base,cgns_zone,'Ilo',BCInflow,PointRange,2,ibc(:,:,1),cgns_bc,ierror)
+
+!!! EAST OUTFLOW
+ibc(1,1,2)  = imax - 1
+ibc(2,1,2)  = 1
+ibc(3,1,2)  = 1
+ibc(1,2,2)  = imax - 1
+ibc(2,2,2)  = jmax - 1
+ibc(3,2,2)  = kmax - 1
+call cg_boco_write_f(cgns_file,cgns_base,cgns_zone,'Ihi',BCOutflow,PointRange,2,ibc(:,:,2),cgns_bc,ierror)
+
+!!! SOUTH WALL
+ibc(1,1,3)  = 1
+ibc(2,1,3)  = 1
+ibc(3,1,3)  = 1
+ibc(1,2,3)  = imax - 1
+ibc(2,2,3)  = 1
+ibc(3,2,3)  = kmax - 1
+call cg_boco_write_f(cgns_file,cgns_base,cgns_zone,'Jlo',BCWall,PointRange,2,ibc(:,:,3),cgns_bc,ierror)
+
+!!! WEST INFLOW
+ibc(1,1,4)  = 1
+ibc(2,1,4)  = jmax - 1
+ibc(3,1,4)  = 1
+ibc(1,2,4)  = imax - 1
+ibc(2,2,4)  = jmax - 1
+ibc(3,2,4)  = kmax - 1
+call cg_boco_write_f(cgns_file,cgns_base,cgns_zone,'Jhi',BCOutflow,PointRange,2,ibc(:,:,4),cgns_bc,ierror)
+
+!!! WEST INFLOW
+ibc(1,1,5)  = 1
+ibc(2,1,5)  = 1
+ibc(3,1,5)  = 1
+ibc(1,2,5)  = imax - 1
+ibc(2,2,5)  = jmax - 1
+ibc(3,2,5)  = 1
+call cg_boco_write_f(cgns_file,cgns_base,cgns_zone,'Klo',BCSymmetryPlane,PointRange,2,ibc(:,:,5),cgns_bc,ierror)
+
+!!! WEST INFLOW
+ibc(1,1,6)  = 1
+ibc(2,1,6)  = 1
+ibc(3,1,6)  = kmax - 1
+ibc(1,2,6)  = imax - 1
+ibc(2,2,6)  = jmax - 1
+ibc(3,2,6)  = kmax - 1
+call cg_boco_write_f(cgns_file,cgns_base,cgns_zone,'Khi',BCSymmetryPlane,PointRange,2,ibc(:,:,6),cgns_bc,ierror)
 call cg_close_f(cgns_file,ierror)
 
 if (ierror /= CG_OK) call cg_error_exit_f()
